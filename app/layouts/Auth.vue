bd551776e04aec5321d2f84b47d90d3398016812
Henrique <henrique@sidekick-coder.com>
2025-07-19 16:24:03 -0300

    feat: initial login page

---
+++ app/layouts/Auth.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import LogoIcon from '#app/components/LogoIcon.vue'
+
+// import CLogoIcon from '@/components/CLogoIcon.vue'
+// import { Link, usePage } from '@inertiajs/vue3'
+
+// const page = usePage()
+const name = 'Sidekick Coder'
+const quote = {
+    author: 'Sidekick Coder Team',
+    title: 'Empowering Developers',
+    message: 'Build, test, and deploy your applications with ease.',
+}
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>()
+</script>
+
+<template>
+    <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+            <div class="absolute inset-0 bg-zinc-900" />
+            <img
+                src="/login-bg.jpg"
+                alt="Background Image"
+                class="absolute inset-0 h-full w-full object-cover opacity-50"
+            >
+            <RouterLink
+                to="/"
+                class="relative z-20 flex items-center text-lg font-medium"
+            >
+                {{ name }}
+            </RouterLink>
+
+            <div
+                v-if="quote"
+                class="relative z-20 mt-auto"
+            >
+                <blockquote class="space-y-2">
+                    <p class="text-lg">
+                        &ldquo;{{ quote.message }}&rdquo;
+                    </p>
+                    <footer class="text-sm text-neutral-300">
+                        {{ quote.author }}
+                    </footer>
+                </blockquote>
+            </div>
+        </div>
+        <div class="lg:p-8">
+            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div class="flex flex-col space-y-2 text-center">
+                    <h1
+                        v-if="title"
+                        class="text-xl font-medium tracking-tight"
+                    >
+                        {{ title }}
+                    </h1>
+                    <p
+                        v-if="description"
+                        class="text-sm text-muted-foreground"
+                    >
+                        {{ description }}
+                    </p>
+                </div>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ app/pages/auth/login.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+// import InputError from '@/components/InputError.vue'
+// import TextLink from '@/components/TextLink.vue'
+import AuthLayout from '#app/layouts/Auth.vue'
+import { Button } from '#app/components/ui/button'
+import { Checkbox } from '#app/components/ui/checkbox'
+import { Input } from '#app/components/ui/input'
+import { Label } from '#app/components/ui/label'
+import { LoaderCircle } from 'lucide-vue-next'
+
+defineProps<{
+    status?: string;
+    canResetPassword: boolean;
+}>()
+
+// const form = useForm({
+//     email: '',
+//     password: '',
+//     remember: false,
+// })
+
+const submit = () => {
+    // form.post(route('login'), {
+    //     onFinish: () => form.reset('password'),
+    // })
+}
+</script>
+
+<template>
+    <AuthLayout
+        title="Log in to your account"
+        description="Enter your email and password below to log in"
+    >
+        <!-- <Head title="Log in" /> -->
+
+        <div
+            v-if="status"
+            class="mb-4 text-center text-sm font-medium text-green-600"
+        >
+            {{ status }}
+        </div>
+
+        <form
+            class="flex flex-col gap-6"
+            @submit.prevent="submit"
+        >
+            <div class="grid gap-6">
+                <div class="grid gap-2">
+                    <Label for="email">Email address</Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        autofocus
+                        :tabindex="1"
+                        autocomplete="email"
+                        placeholder="email@example.com"
+                    />
+                </div>
+
+                <div class="grid gap-2">
+                    <div class="flex items-center justify-between">
+                        <Label for="password">Password</Label>
+                        <RouterLink
+                            v-if="canResetPassword"
+                            to="/auth/reset-password"
+                            class="text-sm"
+                            :tabindex="5"
+                        >
+                            Forgot password?
+                        </RouterLink>
+                    </div>
+                    <Input
+                        id="password"
+                        type="password"
+                        required
+                        :tabindex="2"
+                        autocomplete="current-password"
+                        placeholder="Password"
+                    />
+                </div>
+
+                <Button
+                    type="submit"
+                    class="mt-4 w-full"
+                    :tabindex="4"
+                >
+                    Log in
+                </Button>
+            </div>
+        </form>
+    </AuthLayout>
+</template>
