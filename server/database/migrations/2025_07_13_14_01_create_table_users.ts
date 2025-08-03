d5629324e04453d611f4534a7782ee0a673186e7
Henrique <henrique@sidekick-coder.com>
2025-08-03 17:20:12 -0300

    feat: enhance user table schema with unique constraints and timestamps

--- server/database/migrations/2025_07_13_14_01_create_table_users.ts
+++ server/database/migrations/2025_07_13_14_01_create_table_users.ts
@@ -1,15 +1,25 @@
-import { Kysely } from 'kysely'
-import { withSoftDelete, withTimestamps } from '../common.ts'
+import { Kysely, sql } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
-    db.schema.createTable('users')
+    await db.schema.createTable('users')
         .addColumn('id', 'integer', col => col.primaryKey())
         .addColumn('name', 'text')
-        .addColumn('username', 'text', col => col.notNull().unique())
+        .addColumn('username', 'text', col => col.notNull())
         .addColumn('email', 'text', col => col.notNull())
         .addColumn('password', 'text', col => col.notNull())
-        .$call(withTimestamps)
-        .$call(withSoftDelete)
+        .addColumn('created_at', 'timestamp', (col) =>  col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
+        .addColumn('updated_at', 'timestamp', (col) =>  col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
+        .addColumn('deleted_at', 'timestamp')
+        .addUniqueConstraint('users_username_deleted_at_unique', ['username', 'deleted_at'])
+        .addUniqueConstraint('users_email_deleted_at_unique', ['email', 'deleted_at'])
+        .execute()
+
+    // Add a partial unique index so usernames are unique only if deleted_at IS NULL
+    await db.schema
+        .createIndex('users_username_unique_active')
+        .unique()
+        .on('users')
+        .columns(['username', 'deleted_at'])
         .execute()
 }
 
