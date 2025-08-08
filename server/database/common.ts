3a606e795c837d6a513dc017261cf6e1e68fe1b3
Henrique <henrique@sidekick-coder.com>
2025-08-08 16:38:04 -0300

    fix: update type definitions for WithTimestamp and WithSoftDelete interfaces

--- server/database/common.ts
+++ server/database/common.ts
@@ -1,13 +1,16 @@
 import { CreateTableBuilder, sql  } from 'kysely'
-import type { ColumnType, Generated } from 'kysely'
+import type { ColumnType, UpdateQueryBuilder } from 'kysely'
+import type { Database } from '#server/database/types.ts'
 
 export interface WithTimestamp {
-  created_at: ColumnType<Date, string, never>
-  updated_at: ColumnType<Date, string, Date | string>
+  created_at: ColumnType<Date, string | undefined, never>
+  updated_at: ColumnType<Date, string | undefined, Date | string>
 }
 export interface WithSoftDelete {
-  deleted_at: ColumnType<Date | null, string | null, string | null>
+  deleted_at: ColumnType<Date | null, never | null | ReturnType<typeof now>, string | null | ReturnType<typeof now>>
 }
+
+export const now = ()  => sql`CURRENT_TIMESTAMP`
 
 export const withTimestamps = (ctb: CreateTableBuilder<any, any>) => {
     return ctb
