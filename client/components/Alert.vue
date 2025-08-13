04308f3cfbdf5ec2c0bc786b10329fd3dad18835
Henrique <henrique@sidekick-coder.com>
2025-08-13 13:14:02 -0300

    feat: add Alert component with confirmation dialog functionality

---
+++ client/components/Alert.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import Button from './Button.vue'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '#client/components/ui/alert-dialog'
+import { $t } from '#shared/lang.ts'
+
+defineProps({
+    title: {
+        type: String,
+        default: $t('Are you sure?')
+    },
+    description: {
+        type: String,
+        default: $t('This action cannot be undone.')
+    }
+})
+
+const emit = defineEmits<{
+    confirm: []
+}>()
+
+</script>
+
+<template>
+    <AlertDialog>
+        <AlertDialogTrigger>
+            <slot />
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>{{ title }}</AlertDialogTitle>
+                <AlertDialogDescription>
+                    {{ description }}
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel>{{ $t('Cancel') }}</AlertDialogCancel>
+                <AlertDialogAction @click="emit('confirm')">
+                    {{ $t('Confirm') }}
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+</template>
