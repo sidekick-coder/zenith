1dd30ea6b271304b214c63631c721b77dc4c8b1c
Henrique <henrique@sidekick-coder.com>
2025-08-14 13:41:25 -0300

    feat: remove console folder

--- arte.ts
+++ arte.ts
@@ -2,6 +2,6 @@ import { program } from 'commander'
 import { importAll } from '#server/utils/importAll.ts'
 import { basePath } from '#server/utils/paths.ts'
 
-await importAll(basePath('console/commands'))
+await importAll(basePath('server/commands'))
 
 program.parse()
