e4c4d4114437fa48602957d6206e19155ec6a778
Henrique <henrique@sidekick-coder.com>
2025-09-23 20:51:39 -0300

    feat: roles list page

---
+++ client/pages/admin/roles/index.vue
@@ -0,0 +1,153 @@
+<script setup lang="ts">
+import { watch, ref } from 'vue'
+import { toast } from 'vue-sonner'
+import { useRouter } from 'vue-router'
+import DataTable, { defineColumns } from '#client/components/DataTable.vue'
+import { $t } from '#shared/lang.ts'
+import AppLayout from '#client/layouts/AppLayout.vue'
+import { $fetch } from '#client/utils/fetcher.ts'
+import { tryCatch } from '#shared/tryCatch.ts'
+import Button from '#client/components/Button.vue'
+import Icon from '#client/components/Icon.vue'
+import AlertButton from '#client/components/AlertButton.vue'
+import type Role from '#shared/entities/role.entity.ts'
+import { createId } from '#client/utils/createId.ts'
+
+const router = useRouter()
+
+const items = ref<Role[]>([])
+const loading = ref(false)
+const saving = ref(false)
+const page = ref(1)
+const deletingItems = ref<number[]>([])
+
+const columns = defineColumns([
+    {
+        id: 'id',
+        header: 'ID',
+        accessorKey: 'id',
+        size: 50,
+        minSize: 50,
+        maxSize: 100,
+    },
+    {
+        id: 'name',
+        header: $t('Name'),
+        accessorKey: 'name'
+    },
+    {
+        id: 'description',
+        header: $t('Description'),
+        accessorKey: 'description'
+    },
+    { id: 'actions' }
+])
+
+async function load(){
+    loading.value = true 
+
+    const [error, response] = await tryCatch(() => $fetch<{ data: Role[] }>('/api/roles', {
+        method: 'GET',
+        query: {
+            page: page.value,
+            limit: 20,
+        },
+    }))
+
+    if (error) {
+        loading.value = false
+        return
+    }
+
+    items.value = response.data || []
+
+    setTimeout(() => {
+        loading.value = false
+    }, 500)
+}
+
+async function create() {
+    saving.value = true
+
+    const [error, response] = await tryCatch(() => $fetch<Role>('/api/roles', {
+        method: 'POST',
+        data: { name: 'New Role' + createId(), } 
+    }))
+
+    if (error) {
+        saving.value = false
+        return
+    }
+
+    toast.success($t('Created successfully.'))
+    
+    setTimeout(() => {
+        saving.value = false
+        router.push(`/admin/roles/${response.id}`)
+    }, 1000)
+}
+
+async function destroy(id: number) {
+    deletingItems.value.push(id)
+
+    const [error] = await tryCatch(() => $fetch(`/api/roles/${id}`, { method: 'DELETE', }))
+
+    if (error) {
+        deletingItems.value = []
+        return
+    }
+
+    setTimeout(() => {
+        toast.success($t('Deleted successfully.'))
+        deletingItems.value = []
+        items.value = items.value.filter(i => i.id !== id)
+    }, 1000)
+
+}
+
+watch(page, load, { immediate: true })
+</script>
+<template>
+    <AppLayout>
+        <div class="flex">
+            <h1 class="text-2xl font-bold mb-4 text-foreground flex-1">
+                {{ $t('Roles') }}
+            </h1>
+            <div>
+                <Button
+                    :loading="saving"
+                    @click="create"
+                >
+                    {{ $t('Add new') }}
+                </Button>
+            </div>
+        </div>
+
+        <DataTable 
+            :rows="items"
+            :page="page"
+            :columns="columns"
+            :loading="loading"
+        >
+            <template #row-actions="{ row }">
+                <div class="flex items-center gap-2 justify-end">
+                    <Button
+                        variant="ghost"
+                        :to="`/admin/roles/${row.id}`"
+                        size="sm"
+                    >
+                        <Icon name="pencil" />
+                    </Button>
+                    <AlertButton
+                        variant="ghost"
+                        size="sm"
+                        :loading="deletingItems.includes(row.id)"
+                        @confirm="destroy(row.id)"
+                    >
+                        <Icon name="trash" />
+                    </AlertButton>
+                </div>
+            </template>
+        </DataTable>
+    </AppLayout>
+</template>
--- server/routes/roles.route.ts
+++ server/routes/roles.route.ts
@@ -1,14 +1,79 @@
+import BaseException from '#server/exceptions/base.ts'
+import db from '#server/facades/db.facade.ts'
 import rootRouter from '#server/facades/router.facade.ts'
 import authMiddleware from '#server/middlewares/auth.middleware.ts'
 import { list } from '#server/queries/index.ts'
 import Role from '#shared/entities/role.entity.ts'
+import validator from '#shared/services/validator.service.ts'
 
 const router = rootRouter.use(authMiddleware)
     .prefix('/api/roles')
     .group()
 
 router.get('/', async () => {
-    const rows = await list('roles', { serialize: r => new Role(r) })
+    const data = await list('roles', { serialize: r => new Role(r) })
 
-    return rows
+    return { data }
+})
+
+router.get('/:id', async ({ params }) => {
+    const row = await db.selectFrom('roles')
+        .selectAll()
+        .where('id', '=', Number(params.id))
+        .executeTakeFirst()
+
+    if (!row) {
+        throw new BaseException('Role not found', 404)
+    }
+
+    return new Role(row)
+})
+
+router.post('/', async ({ body }) => {
+    const payload = validator.validate(body, (v) => v.object({
+        name: v.pipe(v.string(), v.minLength(3)),
+        description: v.optional(v.string()),
+    }))
+
+    const row = await db.insertInto('roles').values(payload)
+        .returningAll()
+        .executeTakeFirst()
+
+    if (!row) {
+        throw new BaseException('Failed to create role', 500)
+    }
+
+    return new Role(row)
+})
+
+router.patch('/:id', async ({ params, body }) => {
+    const payload = validator.validate(body, (v) => v.object({
+        name: v.optional(v.pipe(v.string(), v.minLength(3))),
+        description: v.optional(v.string()),
+    }))
+
+    const row = await db.updateTable('roles')
+        .set(payload)
+        .where('id', '=', Number(params.id))
+        .returningAll()
+        .executeTakeFirst()
+
+    if (!row) {
+        throw new BaseException('Failed to update role', 500)
+    }
+
+    return new Role(row)
+})
+
+router.delete('/:id', async ({ params }) => {
+    const row = await db.deleteFrom('roles')
+        .where('id', '=', Number(params.id))
+        .returningAll()
+        .executeTakeFirst()
+
+    if (!row) {
+        throw new BaseException('Failed to delete role', 500)
+    }
+
+    return new Role(row)
 })
