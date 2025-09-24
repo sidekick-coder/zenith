6559a039a5c31d971190d1dbb2498f28154ed662
Henrique <henrique@sidekick-coder.com>
2025-09-24 11:30:55 -0300

    feat: enhance list function with customizable query options

--- server/queries/list.ts
+++ server/queries/list.ts
@@ -2,8 +2,11 @@ import type { Selectable } from 'kysely'
 import type { Database } from '#server/contracts/database.contract.ts'
 import db from '#server/facades/db.facade.ts'
 
+type SelectFrom<T extends keyof Database> = ReturnType<typeof db.selectFrom<T>>
+
 export interface ListOptions<T extends keyof Database> {
     serialize?: (row: Selectable<Database[T]>) => any
+    query?: (qb: SelectFrom<T>) => SelectFrom<T>
 }
 
 export type ListResult<T extends keyof Database, O extends ListOptions<T> | undefined> =
@@ -11,8 +14,11 @@ export type ListResult<T extends keyof Database, O extends ListOptions<T> | unde
     O extends { serialize: (row: Selectable<Database[T]>) => infer R } ? R[] : Selectable<Database[T]>[]
 
 export async function list<T extends keyof Database, O extends ListOptions<T>>(table: T, options?: O): Promise<ListResult<T, O>> {
-    let rows: any[] = await db.selectFrom(table).selectAll()
-        .execute()
+    const query = options?.query 
+        ? options.query(db.selectFrom(table)) 
+        : db.selectFrom(table).selectAll()
+
+    let rows: any[] = await query.execute()
 
     if (options?.serialize) {
         rows = rows.map(options.serialize)
