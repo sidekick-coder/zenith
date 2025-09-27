9c49484c902949b4ef76a8d91e0c25d8da7ab78c
Henrique <henrique@sidekick-coder.com>
2025-09-27 17:51:28 -0300

    feat: enhance make:index command to support object and re-export modes

--- server/commands/makeIndex.ts
+++ server/commands/makeIndex.ts
@@ -3,12 +3,37 @@ import path from 'path'
 import { program } from 'commander'
 import { camelCase } from 'lodash-es'
 
+function makeObjectBarrelIndex(files: string[], name = 'all'): string {
+    // Generate imports
+    const imports = files.map(file => `import * as ${camelCase(file.split('.')[0])} from './${file}'`).join('\n')
+        
+    // Generate const object with all imports
+    const objectEntries = files.map(file => `    ${camelCase(file.split('.')[0])}`).join('\n')
+        
+    const indexContent = [
+        `${imports}`,
+        '', 
+        `const ${name} = {`,
+        `${objectEntries}`,
+        '}', 
+        '',
+        `export default ${name}`,
+        '']
+        .join('\n')
+        
+    return indexContent
+}
+function makeReExportBarrelIndex(files: string[]): string {
+    // Generate re-exports
+    return files.map(file => `export * from './${file}'`).join('\n')
+}
+
 program.command('make:index')
     .requiredOption('-d, --directory <directory>', 'Directory to where generate index.ts file')
-    .option('-n, --name <name>', 'Name of export object', 'schemas')
-    .option('--strip-suffix <stripSuffix>', 'Strip suffix from import names')
+    .option('-m --mode', 'Mode of index file: "object" or "re-export"', 're-export')
+    .option('-n, --name <name>', 'Name of export object', 'all')
     .action(async (options) => {
-        const { directory, name, stripSuffix } = options
+        const { directory, name, mode } = options
         
         // Check if directory exists
         if (!fs.existsSync(directory)) {
@@ -28,31 +53,19 @@ program.command('make:index')
             console.log('No TypeScript files found to import.')
             return
         }
+
+        let content = ''
+
+        if (mode === 'object') {
+            content = makeObjectBarrelIndex(files, name)
+        } 
+
+        if (mode === 're-export') {
+            content =makeReExportBarrelIndex(files)
+        }       
+
+        fs.writeFileSync(path.join(directory, 'index.ts'), content, 'utf-8')
         
-        // Generate imports
-        const imports = files.map(file => {
-            return `import * as ${camelCase(file.split('.')[0])} from './${file}'`
-        }).join('\n')
-        
-        // Generate const object with all imports
-        const objectEntries = files.map(file => {
-            return `    ${camelCase(file.split('.')[0])},`
-        }).join('\n')
-        
-        const indexContent = [
-            `${imports}`,
-            '', 
-            `const ${name} = {`,
-            `${objectEntries}`,
-            '}', 
-            '',
-            `export default ${name}`,
-            '']
-            .join('\n')
-        
-        // Write index.ts file
-        const indexPath = path.join(directory, 'index.ts')
-        fs.writeFileSync(indexPath, indexContent)
         
         console.log(`✅ Generated index.ts in ${directory}`)
         console.log(`📝 Imported ${files.length} files into "${name}" object`)
