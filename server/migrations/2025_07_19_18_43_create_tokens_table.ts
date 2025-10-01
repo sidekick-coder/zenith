63cbd36e8aa1f81cd0fff1229fd17e649cc475b3
Henrique <henrique@sidekick-coder.com>
2025-09-30 21:49:57 -0300

    feat: enhance user_id column definition with cascading delete

--- server/migrations/2025_07_19_18_43_create_tokens_table.ts
+++ server/migrations/2025_07_19_18_43_create_tokens_table.ts
@@ -4,7 +4,13 @@ import { addTimestampColumns } from '#server/queries/index.ts'
 export async function up(db: Kysely<any>): Promise<void> {
     await db.schema.createTable('tokens')
         .addColumn('id', 'integer', col => col.primaryKey())
-        .addColumn('user_id', 'integer', col => col.notNull().references('users.id'))
+        .addColumn('user_id', 
+            'integer', 
+            col => col
+                .notNull()
+                .references('users.id')
+                .onDelete('cascade')
+        )
         .addColumn('token', 'text', col => col.notNull().unique())
         .addColumn('type', 'text', col => col.notNull().defaultTo('auth'))
         .addColumn('expires_at', 'timestamp')
