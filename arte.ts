705a7ec57eb3cc45938d2934ca5349b92b08df13
Henrique <henrique@sidekick-coder.com>
2025-10-03 19:55:58 -0300

    feat: add module loading functionality to dynamically import server commands

--- arte.ts
+++ arte.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import { program } from 'commander'
 import { importAll } from '#server/utils/importAll.ts'
 import { basePath } from '#server/utils/paths.ts'
@@ -13,5 +14,22 @@ await config.load()
 await db.load(undefined, true)
 
 await importAll(basePath('server/commands'))
+
+const modulesPath = basePath('modules')
+const moduleNames = fs.readdirSync(modulesPath, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+for await (const name of moduleNames) {
+    const enabled = config.get(`modules.enabled.${name}`, false)
+    
+    if (!enabled) {
+        continue
+    }
+    
+    if (fs.existsSync(`${modulesPath}/${name}/server/commands`)) {
+        await importAll(`${modulesPath}/${name}/server/commands`)
+    }
+}
 
 program.parse()
