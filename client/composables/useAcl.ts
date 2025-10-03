47e8fbd795026d0f410f97f53fee3e5ed8f09a40
Henrique <henrique@sidekick-coder.com>
2025-10-03 20:14:58 -0300

    refactor: simplify user watch function in useAcl composable

--- client/composables/useAcl.ts
+++ client/composables/useAcl.ts
@@ -12,12 +12,13 @@ const cannot: Acl['cannot'] = (...args) => {
     return acl.value.cannot(...args)
 }
 
-watch(() => $auth.user, (newUser) => {
-    acl.value = new Acl(newUser?.permissions || [])
+watch(() => $auth.user, (u) => {
+    acl.value = new Acl(u?.permissions || [])
 }, { immediate: true })
 
 
 export const $acl = reactive({
+    permissions: computed(() => acl.value.permissions || []),
     ability: computed(() => acl.value.ability),
     can,
     cannot,
