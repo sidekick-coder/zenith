fb581c82e7b55b401ef2d8a5d6bebca988b45fbf
Henrique <henrique@sidekick-coder.com>
2025-10-03 12:15:52 -0300

    feat: add Image component with dialog functionality

---
+++ client/components/Image.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+} from '#client/components/ui/dialog'
+
+defineProps({
+    src: {
+        type: String,
+        required: true,
+    },
+    alt: {
+        type: String,
+        default: null,
+    },
+})
+
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <img
+                :src="src"
+                :alt="alt"
+                v-bind="$attrs"
+            >
+        </DialogTrigger>
+            
+        <DialogContent 
+            class="sm:max-w-none w-auto h-auto p-2"
+            hide-close
+        >
+            <img
+                :src="src"
+                :alt="alt"
+                class="block w-[100dvw] max-w-[80dvw] max-h-[80dvh] rounded-lg object-cover"
+            >
+        </DialogContent>
+    </Dialog>
+</template>
