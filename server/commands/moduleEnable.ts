1707434bca1a989ece1ac26da510537684ae8185
Henrique <henrique@sidekick-coder.com>
2025-10-11 12:17:13 -0300

    feat: add build option to module:enable command for conditional building

--- server/commands/moduleEnable.ts
+++ server/commands/moduleEnable.ts
@@ -1,16 +1,19 @@
 import { program } from 'commander'
 import modules from '#server/services/modules.service.ts'
-import env from '#server/env.ts'
 import build from '#server/services/build.service.ts'
 import config from '#server/facades/config.facade.ts'
 
 program.command('module:enable')
     .helpGroup('module')
     .argument('<module>', 'Module to enable')
-    .action(async (name) => {
+    .option('-b, --build', 'Build after enabling the module', false)
+    .action(async (name, options) => {
         await config.load()
 
-        await modules.enable(name, { build: env.isProduction })
+        await modules.enable(name)
+
+        if (options.build){
+            await build.build()
+        }
         
-        await build.build()
     })
