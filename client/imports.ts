250a4145b92fce12fc238722f0e99e8453527254
Henrique <henrique@sidekick-coder.com>
2025-10-25 14:56:27 -0300

    fix: recursive vue imports

--- client/imports.ts
+++ client/imports.ts
@@ -10,38 +10,33 @@ globalThis.imports = {
     'vee-validate': veeValidate,
 }
 
-// Handle #client and #shared imports 
-const files = {}
-
-Object.assign(files, import.meta.glob('./utils/*', { eager: true }))
-
-for (const [path, mod] of Object.entries(import.meta.glob('./utils/*', { eager: true }))) {
+// Handle #client and #shared imports
+for (const [path, mod] of Object.entries(import.meta.glob('./{utils,components}/**/*.ts', { eager: true }))) {
     const id = path.replace('./', '#client/')
 
     globalThis.imports[id] = mod
     globalThis.imports[id.replace('.ts', '')] = mod
-}
 
-for (const [path, mod] of Object.entries(import.meta.glob<any>('./layouts/*.vue', { eager: true }))) {
-    const id = path.replace('./', '#client/')
-
-    globalThis.imports[id] = mod.default || mod
-    globalThis.imports[id.replace('.vue', '')] = mod.default || mod
-}
-
-for (const [path, mod] of Object.entries(import.meta.glob<any>('./components/**/*', { eager: true }))) {
-    const id = path.replace('./', '#client/')
-
-    const ext = path.split('.').pop()
-    
-    globalThis.imports[id] = mod.default || mod
-    globalThis.imports[id.replace(`.${ext}`, '')] = mod.default || mod
-    
-    if (id.endsWith('index.ts')) {
+    if (path.endsWith('index.ts')) {
         const indexId = id.replace('/index.ts', '')
 
-        globalThis.imports[indexId] = mod.default || mod
+        globalThis.imports[indexId] = mod
     }
+}
+
+for (const [path, importFn] of Object.entries(import.meta.glob<any>('./{components,layouts}/**/*.vue'))) {
+    const id = path.replace('./', '#client/')
+
+    const lazyImport = Vue.defineAsyncComponent(async () => {
+        const mod = await importFn()
+
+        return mod.default || mod
+    })
+
+    const ext = path.split('.').pop()
+
+    globalThis.imports[id] = lazyImport
+    globalThis.imports[id.replace(`.${ext}`, '')] = lazyImport
 }
 
 for (const [path, mod] of Object.entries(import.meta.glob('./../shared/**', { eager: true }))) {
