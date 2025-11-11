2709f03c73ea5b110996ec495435e27c03557565
Henrique <henrique@sidekick-coder.com>
2025-11-11 11:06:27 -0300

    feat: add seed creation command and seed template

---
+++ server/commands/seedMake.ts
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import path from 'path'
+import { program } from 'commander'
+import { basePath } from '#server/utils/paths.ts'
+import template from '#server/services/template.service.ts'
+import modules from '#server/services/modules.service.ts'
+
+program.command('seed:make')
+    .argument('<name>', 'Seed name')
+    .option('-m, --module <module>', 'Module name')
+    .action(async (name, options) => {
+        const seedName = `${name}.seed.ts`
+
+        let filename = basePath('server', 'seeds', seedName)
+
+        if (options.module) {
+            const mod = await modules.findOrFail(options.module)
+
+            filename = mod.makePath('server', 'seeds', seedName)
+        }
+
+        const contents = await template.fromFile(basePath('server', 'templates', 'seed.ts'))
+
+        if (!fs.existsSync(path.dirname(filename))) {
+            await fs.promises.mkdir(path.dirname(filename), { recursive: true })
+        }
+
+        fs.writeFileSync(filename, contents)
+
+        console.log(`Seed created: ${path.relative(basePath(), filename)}`)
+    })
