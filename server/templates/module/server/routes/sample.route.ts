24664283d6922e1a58348dfcfe5f771c97904842
Henrique <henrique@sidekick-coder.com>
2025-11-14 16:25:30 -0300

    fix: update template syntax from <%= name %> to {{ name }} in various files

--- server/templates/module/server/routes/sample.route.ts
+++ server/templates/module/server/routes/sample.route.ts
@@ -1,6 +1,6 @@
 import router from '#server/facades/router.facade.ts'
 
-router.post('/api/<%= name %>/home', async () => {
+router.post('/api/{{ name }}/home', async () => {
     return {
         message: 'This route has been disabled.'
     }
