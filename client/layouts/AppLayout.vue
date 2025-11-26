5e742c703ee7e2fae52b2d9588271dea83fc7e4a
Henrique <henrique@sidekick-coder.com>
2025-11-26 01:12:55 -0300

    feat: add hideBreadcrumbs prop to control breadcrumb visibility

--- client/layouts/AppLayout.vue
+++ client/layouts/AppLayout.vue
@@ -53,6 +53,10 @@ defineProps({
         type: Boolean,
         default: true,
     },
+    hideBreadcrumbs: {
+        type: Boolean,
+        default: false,
+    }
 })
 
 const menu = defineModel('menu', {
@@ -251,6 +255,7 @@ onMounted(() => {
 
         <SidebarInset variant="sidebar">
             <header
+                v-if="!hideBreadcrumbs"
                 class="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4"
             >
                 <div class="flex items-center gap-2">
