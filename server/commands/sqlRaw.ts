30b0327b75edbebf3bd06a8fa8222b18bf906a2d
Henrique <henrique@sidekick-coder.com>
2025-11-27 12:44:14 -0300

    feat: refactor sql:raw command to support multiple SQL statements and improve error handling

--- server/commands/sqlRaw.ts
+++ server/commands/sqlRaw.ts
@@ -4,6 +4,8 @@ import { program } from 'commander'
 import { sql } from 'kysely'
 import db from '#server/facades/db.facade.ts'
 import { tryCatch } from '#shared/utils/tryCatch.ts'
+import cli from '#server/services/cli.service.ts'
+import logger from '#server/facades/logger.facade.ts'
 
 
 program.command('sql:raw')
@@ -11,29 +13,30 @@ program.command('sql:raw')
     .helpGroup('sql')
     .option('-q, --query <query>', 'The raw SQL query to execute')
     .option('-f, --file <file>', 'Path to a file containing the SQL query')
-    .action(async (options) => {
-        let query = options.query
+    .action( cli.with(['db'], async (options) => {
+        let query: string = options.query || ''
 
         if (options.file) {
             const filename = path.resolve(process.cwd(), options.file)
             query = await fs.promises.readFile(filename, 'utf-8')
         }
 
-        const [error, response] = await tryCatch(async () => {
-            await db.load()
+        const statements = query.trim().split(';')
+            .filter(s => s.trim().length > 0)
 
-            const result = await sql.raw(query).execute(db)
+        for (const stmt of statements) {
+            logger.info('Executing query:', stmt)
 
-            await db.destroy()
+            const [error, response] = await tryCatch(() => sql.raw(stmt).execute(db))
+            
+            if (error) {
+                logger.error('Error executing query:', error)
+                return
+            }
 
-            return result
-        })
-
-        if (error) {
-            console.log('Error executing query:', error)
-            return
+            if (response.rows && response.rows.length > 0) {
+                console.log(response.rows)
+            }
         }
 
-        console.log(response)
-
-    })
+    }))
