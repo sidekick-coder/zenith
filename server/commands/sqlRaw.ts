7ddec6eca5191ff721ca7e004ef526fd3254a7c4
Henrique <henrique@sidekick-coder.com>
2025-11-27 12:19:45 -0300

    feat: enhance sql:raw command to support file input for SQL queries

--- server/commands/sqlRaw.ts
+++ server/commands/sqlRaw.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+import fs from 'fs'
 import { program } from 'commander'
 import { sql } from 'kysely'
 import db from '#server/facades/db.facade.ts'
@@ -7,8 +9,16 @@ import { tryCatch } from '#shared/utils/tryCatch.ts'
 program.command('sql:raw')
     .description('Execute a raw SQL query')
     .helpGroup('sql')
-    .arguments('<query>')
-    .action(async (query: string) => {
+    .option('-q, --query <query>', 'The raw SQL query to execute')
+    .option('-f, --file <file>', 'Path to a file containing the SQL query')
+    .action(async (options) => {
+        let query = options.query
+
+        if (options.file) {
+            const filename = path.resolve(process.cwd(), options.file)
+            query = await fs.promises.readFile(filename, 'utf-8')
+        }
+
         const [error, response] = await tryCatch(async () => {
             await db.load()
 
