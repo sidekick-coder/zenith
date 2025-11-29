b1c3758fb8164f186e50de74d981e8f634527b0c
Henrique <henrique@sidekick-coder.com>
2025-11-28 21:20:10 -0300

    feat: enhance File entity with caching for URL retrieval and add DriveUrlOptions type

--- server/entities/file.entity.ts
+++ server/entities/file.entity.ts
@@ -1,9 +1,16 @@
+import ms from 'ms'
 import { Model } from '#server/mixins/model.mixin.ts'
 import Base from '#shared/entities/file.entity.ts'
 import { composeWith } from '#shared/utils/compose.ts'
 import { Metadata } from '#server/mixins/metadata.mixin.ts'
 import drive from '#server/facades/drive.facade.ts'
 import { Hooks } from '#server/mixins/hooks.mixin.ts'
+import type { DriveUrlOptions } from '#server/contracts/drive.contract.ts'
+
+interface URLCache {
+    url: string
+    expires: number
+}
 
 export default class File extends composeWith(
     Base,
@@ -11,6 +18,9 @@ export default class File extends composeWith(
     Model('files'),
     Metadata('file_metas', 'file_id')
 ) {
+
+    public static cache = new Map<string, URLCache>()
+
     public static boot(){
         this.on('serialized', async (file: File) => {
             file.url = await drive.url(file.filename)
@@ -35,8 +45,31 @@ export default class File extends composeWith(
         return drive.use(this.drive).readStream(this.filename)
     }
 
-    public async loadUrl() {
+    public async loadUrl(options: DriveUrlOptions = {}) {
+        const cache = File.cache.get(this.filename)
+        const now = Date.now()
+
+        if (cache && now < cache.expires) {
+            this.url = cache.url
+            return
+        }
+
+        if (!options.expires) {
+            options.expires = '1h'
+        }
+        
         this.url = await drive.use(this.drive).url(this.filename)
+        
+        if (!this.url) {
+            return
+        }
+
+        const expires = now + ms(options.expires)
+
+        File.cache.set(this.filename, {
+            url: this.url,
+            expires
+        })
     }
 
     public async deleteFromDrive() {
