5110dbedc6c905f4f1054e7df8cf062426f61ae4
Henrique <henrique@sidekick-coder.com>
2025-12-07 13:06:44 -0300

    refactor: clean up imports and update build output directories

--- server/services/server.service.ts
+++ server/services/server.service.ts
@@ -1,8 +1,7 @@
-import fs from 'fs'
-import { mergeConfig, build as viteBuild } from 'vite'
-import chokidar from 'chokidar'
+import { mergeConfig, build as viteBuild  } from 'vite'
+import type { UserConfig } from 'vite'
 import { logger } from '#server/facades/logger.facade.ts'
-import { basePath, storagePath } from '#server/utils/paths.ts'
+import { basePath } from '#server/utils/paths.ts'
 import ServerBooterService from '#server/services/serverBooter.service.ts'
 
 export default class ServerService {
@@ -16,7 +15,7 @@ export default class ServerService {
 
     public async build() {
         
-        const common = {
+        const common: UserConfig = {
             publicDir: 'client/public',
             resolve: { 
                 alias: {
@@ -24,18 +23,29 @@ export default class ServerService {
                     '#shared': basePath('shared'),
                 }
             },
+            build: {
+                rollupOptions: {
+                    external: (id: string) => {
+                        if (id.startsWith(basePath('modules'))) {
+                            return true
+                        }
+
+                        return false
+                    }
+                }
+            }
         }
 
         await viteBuild(mergeConfig(common, {
             build: {
                 ssr: 'client/entry-server.ts',
-                outDir: basePath('client-dist/server'),
+                outDir: basePath('client-dist/node'),
             },
         }))
 
         await viteBuild(mergeConfig(common, { 
             build: {
-                outDir: basePath('client-dist/client'),
+                outDir: basePath('client-dist/browser'),
                 rollupOptions: { 
                     input: { app: 'client/index.html', },
                 },
