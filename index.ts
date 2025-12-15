03dfdaf6a2dbd4a4033acd6c9ecf2033e7cbe712
Henrique <henrique@sidekick-coder.com>
2025-12-15 16:55:59 -0300

    fix: missing errors messages from lifecycle

--- index.ts
+++ index.ts
@@ -1,23 +1,39 @@
+import config from '#server/facades/config.facade.ts'
 import di from '#server/facades/di.facade.ts'
 import env from '#server/facades/env.facade.ts'
-import lifecycle from '#server/facades/lifecycle.facade.ts'
 import LoggerWinsonService from '#server/services/loggerWinson.service.ts'
 import { importAll } from '#server/utils/importAll.ts'
 import { basePath } from '#server/utils/paths.ts'
 import LifecycleHook from '#shared/entities/lifecycleHook.entity.ts'
+import LifecycleService from '#shared/services/lifecycle.service.ts'
 import LoggerService from '#shared/services/logger.service.ts'
+
+// handle unhandled rejections
+process.on('unhandledRejection', (reason: any, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+})
+
+process.on('uncaughtException', (error: Error) => {
+    console.error('Uncaught Exception:', error)
+})
 
 env.load()
 
-di.set(LoggerService, LoggerWinsonService.create({
+const logger = LoggerWinsonService.create({
     level: env.get('LOG_LEVEL', 'info'),
     transports: [
         LoggerWinsonService.console(),
         LoggerWinsonService.file(basePath('storage/logs/error.log'), 'error'),
         LoggerWinsonService.file(basePath('storage/logs/combined.log')),
     ]
-}))
+})
 
+di.set(LoggerService, logger)
+
+const lifecycle = new LifecycleService({
+    debug: config.get('lifecycle.debug') || config.get('app.debug'),
+    logger: logger.child({ label: 'lifecycle' }),
+})
 
 const mods = await importAll(basePath('server/hooks'))
 
