28a108d02cb791a1011a856056c81572fd46e75b
Henrique <henrique@sidekick-coder.com>
2025-12-26 17:29:15 -0300

    refactor: enhance error handling in Acl.authorize method to include action and subject details

--- server/entities/acl.entity.ts
+++ server/entities/acl.entity.ts
@@ -4,7 +4,28 @@ import BaseException from '#server/exceptions/base.ts'
 export default class Acl extends BaseAcl {
     public authorize(action: string, subject: any, object?: Record<string, any>) {
         if (this.cannot(action, subject, object)) {
-            throw new BaseException('Unauthorized', 403)
+            const error = new BaseException('Unauthorized', 403)
+
+            let subjectName = ''
+            
+            if (typeof subject === 'string') {
+                subjectName = subject
+            }
+
+            if (subject && typeof subject === 'function' && subject.name) {
+                subjectName = subject.name
+            }
+
+            if (subject && typeof subject === 'object' && subject.constructor && subject.constructor.name) {
+                subjectName = subject.constructor.name
+            }
+
+            Object.assign(error, {
+                action,
+                subject: subjectName,
+            })
+            
+            throw error
         }
     }
 }
