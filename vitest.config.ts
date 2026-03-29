f9b7a54f6530abcd38c02c72b7be296590634af2
Henrique <henrique@sidekick-coder.com>
2026-03-29 14:50:03 -0300

    refactor: remove test.command.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,34 @@
 // eslint-disable-next-line import/extensions
 import { defineConfig } from 'vitest/config'
 
-
-export default defineConfig({ 
-    test: { 
+export default defineConfig({
+    test: {
         watch: false,
         exclude: [
-            '**/node_modules/**', 
-            '**/dist/**', 
+            '**/node_modules/**',
+            '**/dist/**',
             '**/build/**',
-            '**/coverage/**', 
+            '**/coverage/**',
             '**/logs/**',
             '**/root/**'
         ],
+        projects: [
+            {
+                test: {
+                    name: 'unit',
+                    include: ['**/*.unit.test.ts'],
+                    exclude: ['**/modules/**'],
+                }
+            },
+            {
+                test: {
+                    name: 'integration',
+                    include: ['**/*.int.test.ts'],
+                    exclude: ['**/modules/**'],
+                    testTimeout: 60000, // Increase timeout for integration tests
+                    hookTimeout: 60000, // Increase hook timeout for integration tests
+                }
+            }
+        ]
     },
 })
