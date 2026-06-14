03dc0201bb22ec67ecfcfdd4e565c5983c0b0b56
Henrique <henrique@sidekick-coder.com>
2026-06-14 18:46:06 -0300

    feat: veeValidate facade

--- client/imports.ts
+++ client/imports.ts
@@ -8,7 +8,7 @@ import * as VueSonner from 'vue-sonner'
 import * as VueUse from '@vueuse/core' 
 import * as UnheadVue from '@unhead/vue' 
 import * as UnheadVueComponents from '@unhead/vue/components'
-import { container, VUE_CONTAINER_KEY, VUE_ROUTER_CONTAINER_KEY } from '@sidekick-coder/zenith-kit/client'
+import { container, VUE_CONTAINER_KEY, VUE_ROUTER_CONTAINER_KEY, VEE_VALIDATE_CONTAINER_KEY } from '@sidekick-coder/zenith-kit/client'
 
 globalThis.imports = globalThis.imports || new Map<string, any>()
 
@@ -16,7 +16,6 @@ globalThis.imports.set('vue', () => Promise.resolve(Vue))
 globalThis.imports.set('reka-ui', () => Promise.resolve(RekaUI))
 globalThis.imports.set('vue-router', () => Promise.resolve(VueRouter))
 globalThis.imports.set('vee-validate', () => Promise.resolve(VeeValidate))
-globalThis.imports.set('static:vee-validate', VeeValidate)
 globalThis.imports.set('vue-sonner', () => Promise.resolve(VueSonner))
 globalThis.imports.set('@vueuse/core', () => Promise.resolve(VueUse))
 globalThis.imports.set('@unhead/vue', () => Promise.resolve(UnheadVue))
@@ -33,3 +32,4 @@ globalThis.importAsync = async (id: string): Promise<any> => {
 
 container.set(VUE_CONTAINER_KEY, Vue)
 container.set(VUE_ROUTER_CONTAINER_KEY, VueRouter)
+container.set(VEE_VALIDATE_CONTAINER_KEY, VeeValidate)
